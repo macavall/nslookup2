b200098a464cc74ae63ff105e51efa9f0b778cca
Matthew Cavallo <macavall@microsoft.com>
2023-04-07 13:51:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+
+namespace nslookup2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Do a few lookups by host name and address  
+            DNSLookup(args[0]);
+            //DNSLookup("207.46.16.248");
+
+            // Keep the console window open in debug mode  
+            //Console.WriteLine("Press any key to exit.");
+            //Console.ReadKey();
+        }
+
+        protected static void DNSLookup(string hostNameOrAddress)
+        {
+            Console.WriteLine("Lookup: {0}\n", hostNameOrAddress);
+
+            IPHostEntry hostEntry = Dns.GetHostEntry(hostNameOrAddress);
+            Console.WriteLine("  Host Name: {0}", hostEntry.HostName);
+
+            IPAddress[] ips = hostEntry.AddressList;
+            foreach (IPAddress ip in ips)
+            {
+                Console.WriteLine("  Address: {0}", ip);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
